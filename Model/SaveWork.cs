4ace9745cf066f190b799471d5916841e9a4fef0
OscarPALISSOT <74594715+OscarPALISSOT@users.noreply.github.com>
2021-12-18 22:46:10 +0100

    push on github

---
+++ Model/SaveWork.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using EasySave.Command;
+
+namespace EasySave.Model
+{
+    public class SaveWork
+    {
+        public string Name { get; set; }
+        public SaveInfo Info { get; set; }
+        public SaveState State { get; set; }
+        public bool selected { get; set; }
+        public bool Play { get; set; }
+        public bool Priority { get; set; }
+
+        public SaveWork(string name, string fileSource, string fileTarget, bool type)
+        {
+            Info = new SaveInfo();
+            State = new SaveState();
+            Name = name;
+            Info.FileSource = fileSource;
+            Info.FileTarget = fileTarget;
+            Info.Full = type;
+            State.State = 1;
+            State.Name = name;
+            Info.Name = name;
+            State.TotalFileToCopy = Commands.GetDirectoryTotalNbFile(Info.FileSource);
+            State.TotalDirectorySize = Commands.GetDirectoryTotalSize(Info.FileSource);
+            Play = true;
+            Priority = false;
+        }
+    }
+}
