23907ca47b23549b4e0f854bac0b912c5b330342
OscarPALISSOT <74594715+OscarPALISSOT@users.noreply.github.com>
2021-12-18 22:34:29 +0100

    push on github

---
+++ App.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.IO;
+using System.Threading;
+
+namespace EasySave2._0
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private static Mutex _mutex = null;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            const string appName = "EasySave";
+            bool createdNew;
+
+            _mutex = new Mutex(true, appName, out createdNew);
+
+            if (!createdNew)
+            {
+                //app is already running! Exiting the application
+                MessageBox.Show("Sorry, the app has already been launched...");
+                Application.Current.Shutdown();
+
+            }
+        }
+    }
+}
